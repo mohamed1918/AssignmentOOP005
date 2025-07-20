df89b13004a9ea2a5ac103ee7e67c994fef1831d
mohamed1918 <memoohossam2004@gmail.com>
2025-07-21 02:50:56 +0300

    Q2

---
+++ AssignmentOOP005/IAuthenticationService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOOP005
+{
+    internal interface IAuthenticationService
+    {
+        bool AuthenticateUser(string username, string password);
+
+        bool AuthorizeUser(string username, string role);
+    }
+
+    class BasicAuthenticationService : IAuthenticationService
+    {
+        private Dictionary<string, (string Password, string Role)> users = new()
+        {
+    
+               { "admin", ("1234", "Admin") },
+
+
+               { "user", ("abcd", "User") } 
+        };
+
+        public bool AuthenticateUser(string username, string password)
+        {
+            return users.ContainsKey(username) && users[username].Password == password;
+        }
+
+        public bool AuthorizeUser(string username, string role)
+        {
+            return users.ContainsKey(username) && users[username].Role == role;
+        }
+    }
+
+    
+}
--- AssignmentOOP005/Program.cs
+++ AssignmentOOP005/Program.cs
@@ -6,13 +6,27 @@ namespace AssignmentOOP005
         static void Main(string[] args)
         {
             #region Q1
-            Console.WriteLine("Q1");
-            IShape circle = new Circle(5);
+            //Console.WriteLine("Q1");
+            //IShape circle = new Circle(5);
 
-            IShape rectagle = new Rectangle(5,10);
+            //IShape rectagle = new Rectangle(10,5);
 
-            circle.DisplayShapeInfo();
-            rectagle.DisplayShapeInfo();
+            //circle.DisplayShapeInfo();
+            //rectagle.DisplayShapeInfo();
+            #endregion
+
+            #region Q2
+            IAuthenticationService authService = new BasicAuthenticationService();
+            string username = "admin", password = "1234";
+            if (authService.AuthenticateUser(username, password))
+            {
+                Console.WriteLine("User authenticated successfully.");
+                Console.WriteLine(authService.AuthorizeUser(username, "Admin") ? "User is authorized as Admin." : "User is not authorized.");
+            }
+            else
+            {
+                Console.WriteLine("Authentication failed.");
+            }
             #endregion
         }
     }
