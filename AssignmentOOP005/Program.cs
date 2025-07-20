eaaaee5730cabdc1799f43d50e8a4a28a5a3be14
mohamed1918 <memoohossam2004@gmail.com>
2025-07-21 02:40:47 +0300

    Q1

--- AssignmentOOP005/Program.cs
+++ AssignmentOOP005/Program.cs
@@ -5,7 +5,15 @@ namespace AssignmentOOP005
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            Console.WriteLine("Q1");
+            IShape circle = new Circle(5);
+
+            IShape rectagle = new Rectangle(5,10);
+
+            circle.DisplayShapeInfo();
+            rectagle.DisplayShapeInfo();
+            #endregion
         }
     }
 }
